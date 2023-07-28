bc4fbae11c32ccd8f4108da1c902f6b59ed309b9
apdev176 <rjstud176@gmail.com>
2023-07-28 15:05:56 +0530

    first commit

---
+++ src/main/java/com/jsp/springboot/dto/Student.java
@@ -0,0 +1,17 @@
+package com.jsp.springboot.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+private int id;
+private String name;
+private String email;
+}
